40051d836a20e9a2475e2a2b140a431e286491d2
mch28 <visualdisturbances@hotmail.com>
2017-10-20 00:14:17 +0100

    More preliminary Flask learning. Just a quick cautious commit before trying something on the command line

--- views.py
+++ views.py
@@ -1,10 +1,20 @@
 from flask_app import app
 from models import StationName
+from flask import render_template
 
 
 
 @app.route('/')
-
 def index():
-    first_station = StationName.query.first()
-    return '<h1>The first station is: ' + str(first_station.id) + '</h1>'
+    all_stations = StationName.query.all()
+    one_item = StationName.query.filter_by(id=456).first()
+    return render_template('showfirst.html', one_item=one_item, all_stations=all_stations)
+
+@app.route('/make_name', methods=['POST'])
+def make_name():
+    return render_template('results.html')
+
+
+
+
+
